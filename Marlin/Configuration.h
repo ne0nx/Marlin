edf7f1a8c1e99f40f5e863f238be47dbca406353
Dmitriy Karachentsov <d.karachentsov@gmail.com>
2019-11-08 17:11:38 +0200

    [feature/ghost-1.1.x] Fixed min extrusion temperature

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -454,7 +454,7 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 #define PREVENT_COLD_EXTRUSION
-#define EXTRUDE_MINTEMP 35
+#define EXTRUDE_MINTEMP 175
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
